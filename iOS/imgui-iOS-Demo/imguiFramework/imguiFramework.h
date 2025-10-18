0f7ce4596a1270fea6716d5dbc0b37f79747fa34
andycui <andy380743909@gmail.com>
2025-10-18 16:15:42 +0800

    failed to set framework header file
    I have tried #import <imguiFramework/imgui.h> and other headers #import <imguiFramework/xxx.h>, and add these files in Xcode Build Phases -> Headers -> Public/Project, and also configed the Header Search Path,
    but always failed to compile the framework target. The errors are headers including.

--- iOS/imgui-iOS-Demo/imguiFramework/imguiFramework.h
+++ iOS/imgui-iOS-Demo/imguiFramework/imguiFramework.h
@@ -15,3 +15,6 @@ FOUNDATION_EXPORT const unsigned char imguiFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <imguiFramework/PublicHeader.h>
 
+// TODO: how can i expose public headers?
+// I have tried #import <imguiFramework/imgui.h> and other headers #import <imguiFramework/xxx.h>, and add these files in Xcode Build Phases -> Headers -> Public/Project, and also configed the Header Search Path,
+// but always failed to compile the framework target. The errors are headers including.
